e9c8f94df07129f2a16f51f656eab8b0d245caa9
Simplejack17 <jack.tyburski1@gmail.com>
2018-01-08 13:07:56 -0500

    added front page route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import { Link, Switch, Route, Redirect } from 'react-router-dom'
+
+import FrontPage from './views/frontpage'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <main>
+          <Switch>
+            <Route exact path="/" component={FrontPage}/>
+          </Switch>
+        </main>
       </div>
     );
   }
 }
 
-export default App;
+export default App
---
+++ src/views/frontpage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class FrontPage extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Front Page</h1>
+      </div>
+    )
+  }
+}
+
+
+export default FrontPage
